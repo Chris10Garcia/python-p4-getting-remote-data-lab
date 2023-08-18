be5e45b8d2d13084e0406ce2506e78506fa35f9c
Christian Garcia <bones192nd@gmail.com>
2023-08-18 12:09:37 -0400

    lab submission

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_json = json.loads(self.get_response_body()) #json.loads requires a response.content or response.text input
+        return data_json
+
+
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+# test = GetRequester(url)
+
+# print(json.dumps(test.load_json()))
+
+# https://get.geojs.io/v1/ip/geo.json
